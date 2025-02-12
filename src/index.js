49fed8c9ac0fcb406e2772fb9be6ff643779fdd1
dasartini <adriansartinid@gmail.com>
2025-02-12 18:55:12 +0000

    more debugging

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 console.log("Starting....")
+console.log("DATABASE_URL:",process.env.DATABASE_URL)
+console.log("DB_PORT:",process.env.DB_PORT)
 import express from "express"
 import userRoutes from "./MVC/routes/routes.js"
 import morgan from "morgan"
+import { DB_PORT } from "./config.js"
 
 const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(userRoutes)
 
-const PORT = process.env.PORT || 4000;
+const PORT = DB_PORT
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
