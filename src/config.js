edc93870682b037151b53c52d3e402a71e599220
dasartini <adriansartinid@gmail.com>
2025-02-13 09:56:19 +0000

    refactor3

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ import 'dotenv/config';
 
 export const DATABASE_URL = process.env.DATABASE_URL;
 
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_HOST = process.env.DB_HOST || "db.gcbdhdqvrykesdzcywjb.supabase.co";
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_DATABASE = process.env.DB_DATABASE || "postgres";
-export const DB_PORT = process.env.DB_PORT || 5432;
+// export const DB_USER = process.env.DB_USER || "postgres";
+// export const DB_HOST = process.env.DB_HOST || "db.gcbdhdqvrykesdzcywjb.supabase.co";
+// export const DB_PASSWORD = process.env.DB_PASSWORD;
+// export const DB_DATABASE = process.env.DB_DATABASE || "postgres";
+export const DB_PORT = process.env.DB_PORT|| 5000;
 
-export const PORT = process.env.PORT || 5000;
+// export const PORT = process.env.PORT 
