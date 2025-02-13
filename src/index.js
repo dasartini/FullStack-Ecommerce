8cf92ac57698802fc042fd657c54fbf5e9dfbc3b
dasartini <adriansartinid@gmail.com>
2025-02-13 10:49:50 +0000

    added cors

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import express from "express"
 import userRoutes from "./MVC/routes/routes.js"
 import morgan from "morgan"
 import { DB_PORT } from "./config.js"
+import cors from "cors"
 
 const app = express()
+
+const allowedOrigins = ["https://cafe-tero.netlify.app"];
+app.use(cors({ origin: allowedOrigins }));
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(userRoutes)
